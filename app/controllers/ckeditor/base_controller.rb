2e787fd9dc51ee1e76a77e39b93282a2ea0cf7bb
Rami Grossman <ramigg@gmail.com>
2011-01-23 05:27:50 +0200

    Skip authentication via Devise

--- app/controllers/ckeditor/base_controller.rb
+++ app/controllers/ckeditor/base_controller.rb
@@ -1,5 +1,6 @@
 class Ckeditor::BaseController < ApplicationController
   skip_before_filter :verify_authenticity_token, :only => [:create]
+  skip_before_filter :authenticate_user!, :only => [:create]
   
   before_filter :swf_options, :only => [:index, :create]
   before_filter :find_asset, :only => [:destroy]
